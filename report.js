58ab7aac046263de976ed9a69b58124332683a3b
khundakerfaisal <khundakermis@gmail.com>
2026-03-08 16:29:58 +0600

    aded report j file

---
+++ report.js
@@ -0,0 +1,15 @@
+const newman = require('newman');
+require('dotenv').config();
+newman.run({
+    collection:`https://api.postman.com/collections/52498897-2a2c83bb-6b64-43fa-b675-6accccac4c54?access_key=${process.env.secretKey}`,
+    reporters: 'htmlextra',
+    iterationCount: 1,
+    reporter: {
+        htmlextra: {
+            export: './Reports/report.html', // If not specified, the file will be written to `newman/` in the current working directory.
+        }
+    }
+}, function (err) {
+    if (err) { throw err; }
+    console.log('collection run complete!');
+});
